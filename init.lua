c993bd7fe23ad4cc9df37acc937b996c700cbac7
Neil Viloria <neilzon.viloria@brivity.com>
2024-10-07 14:35:12 -0600

    copy options neotree

--- init.lua
+++ init.lua
@@ -47,7 +47,8 @@ vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true })
 vim.cmd 'set relativenumber number'
 
 -- Change the cursor color to hot pink
-vim.cmd [[highlight Cursor guifg=#FF69B4]]
+-- vim.wo.cursorline = true
+-- vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#ffffff', bg = '#5f87af', bold = true })
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
@@ -715,8 +716,7 @@ local servers = {
   zls = {},
   astro = { filetypes = { 'astro' } },
   clangd = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
-
+  html = { filetypes = { 'html' } },
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
@@ -756,8 +756,8 @@ require('conform').setup {
     python = { 'black', 'isort' },
     json = { 'fixjson' },
     rust = { 'rustfmt' },
-    javascript = { 'prettierd', 'eslint_d' },
-    typescript = { 'prettierd', 'eslint_d' },
+    javascript = { 'prettier', 'prettierd', 'eslint_d' },
+    typescript = { 'prettier', 'prettierd', 'eslint_d' },
     jsx = { 'dprint', 'prettierd' },
     typescriptreact = { 'dprint', 'prettierd' },
     go = { 'gofmt', 'goimports', 'golines' },
@@ -765,8 +765,53 @@ require('conform').setup {
     cpp = { 'clang_format' },
     objcpp = { 'clang_format' },
     objc = { 'clang_format' },
+    html = { 'superhtml' },
+  },
+  format_on_save = {
+    timeout_ms = 1000,
   },
 }
+
+vim.api.nvim_create_user_command('Format', function(args)
+  local range = nil
+  if args.count ~= -1 then
+    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+    range = {
+      start = { args.line1, 0 },
+      ['end'] = { args.line2, end_line:len() },
+    }
+  end
+  require('conform').format { async = true, lsp_format = 'fallback', range = range }
+end, { range = true })
+
+require('conform').setup {
+  format_on_save = function(bufnr)
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+    return { timeout_ms = 500, lsp_format = 'fallback' }
+  end,
+}
+
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = 'Disable autoformat-on-save',
+  bang = true,
+})
+
+vim.api.nvim_create_user_command('FormatEnable', function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = 'Re-enable autoformat-on-save',
+})
 
 vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*',
--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,29 +1,25 @@
 -- Unless you are still migrating, remove the deprecated commands from v1.x
-vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
 
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-    "3rd/image.nvim"
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+    '3rd/image.nvim',
   },
   window = {
     mappings = {
-      ["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true } },
-    }
+      ['P'] = { 'toggle_preview', config = { use_float = false, use_image_nvim = true } },
+    },
   },
   config = function()
-    vim.fn.sign_define("DiagnosticSignError",
-      {text = " ", texthl = "DiagnosticSignError"})
-    vim.fn.sign_define("DiagnosticSignWarn",
-      {text = " ", texthl = "DiagnosticSignWarn"})
-    vim.fn.sign_define("DiagnosticSignInfo",
-      {text = " ", texthl = "DiagnosticSignInfo"})
-    vim.fn.sign_define("DiagnosticSignHint",
-      {text = "󰌵", texthl = "DiagnosticSignHint"})
+    vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError' })
+    vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn' })
+    vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo' })
+    vim.fn.sign_define('DiagnosticSignHint', { text = '󰌵', texthl = 'DiagnosticSignHint' })
 
     require('neo-tree').setup {
       auto_clean_after_session_restore = true,
@@ -34,12 +30,53 @@ return {
           hide_gitignored = false,
         },
         follow_current_file = {
-          enabled = true,          -- This will find and focus the file in the active buffer every time
+          enabled = true, -- This will find and focus the file in the active buffer every time
           --               -- the current file is changed while the tree is open.
           leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
         },
-        use_libuv_file_watcher = true
-      }
+        use_libuv_file_watcher = true,
+      },
+      window = {
+        mappings = {
+          ['Y'] = function(state)
+            -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+            -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            local filename = node.name
+            local modify = vim.fn.fnamemodify
+
+            local results = {
+              filepath,
+              modify(filepath, ':.'),
+              modify(filepath, ':~'),
+              filename,
+              modify(filename, ':r'),
+              modify(filename, ':e'),
+            }
+
+            -- absolute path to clipboard
+            local i = vim.fn.inputlist {
+              'Choose to copy to clipboard:',
+              '1. Absolute path: ' .. results[1],
+              '2. Path relative to CWD: ' .. results[2],
+              '3. Path relative to HOME: ' .. results[3],
+              '4. Filename: ' .. results[4],
+              '5. Filename without extension: ' .. results[5],
+              '6. Extension of the filename: ' .. results[6],
+            }
+
+            if i > 0 then
+              local result = results[i]
+              if not result then
+                return print('Invalid choice: ' .. i)
+              end
+              vim.fn.setreg('"', result)
+              vim.notify('Copied: ' .. result)
+            end
+          end,
+        },
+      },
     }
   end,
 }
